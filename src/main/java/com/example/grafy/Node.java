ee6ddd545fc64c3125737443a34cee072f83e6f8
Adrian Nowosielski <adrian.nowosielski1@gmail.com>
2022-05-26 12:02:51 +0200

    Zmiany klasy

--- src/main/java/com/example/grafy/Node.java
+++ src/main/java/com/example/grafy/Node.java
@@ -1,29 +1,27 @@
 package com.example.grafy;
 
 public class Node implements Comparable<Node> {
-        int numNode; //Default package access
-        double weight;
+    int numNode; //Default package access
+    double weight;
 
-        Node(){
-        }
+    Node(){
+    }
+    Node(int numNode, double distance){
+        this.numNode=numNode;
+        this.weight=distance;
+    }
+    void setNumNodeWeight(int numNode, double weight){
+        this.numNode=numNode;
+        this.weight=weight;
+    }
 
-        Node(int numNode, double distance){
-            this.numNode=numNode;
-            this.weight=distance;
-        }
+    @Override
+    public int compareTo(Node o) {
+        return o.weight>weight ? 1 : 0;
+    }
+    @Override
+    public boolean equals(Object o){
+        return o instanceof Node && (((Node) o).numNode == this.numNode);
+    }
 
-        void setNumNodeWeight(int numNode, double weight){
-            this.numNode=numNode;
-            this.weight=weight;
-        }
-
-        @Override
-        public int compareTo(Node o) {
-            return o.weight>weight ? 1 : 0;
-        }
-
-        @Override
-        public boolean equals(Object o){
-            return o instanceof Node && (((Node) o).numNode == this.numNode);
-        }
 }
